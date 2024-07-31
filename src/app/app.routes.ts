fbe4d2c17447e81f9729bf0148dcd5e5e8f3bc94
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-31 12:50:27 +0200

    routes configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: '/contact', component: ContactComponent},
+    {path: '/products', component: ProductsComponent},
+    {path: '/product/:productId', component: ProductDetailComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
