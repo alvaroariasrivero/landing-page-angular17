63772fa2e15468b13275faae7f72334605c86bde
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-08-01 11:30:35 +0200

    redirect to product page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsApiService } from '../../services/products-api.service';
 import { IProduct } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
@@ -13,11 +15,16 @@ export class ProductsComponent implements OnInit {
 
   productList: IProduct[] = []
   private _apiService = inject(ProductsApiService);
+  private _router = inject(Router)
 
   ngOnInit(): void {
     this._apiService.getProducts().subscribe((data: IProduct[]) => {
       this.productList = data;
     })
+  }
+
+  navigate(productId: number){
+    this._router.navigate(['/product', productId])
   }
 
 }
